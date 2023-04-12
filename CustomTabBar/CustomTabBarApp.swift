fe307df885d60d9a13c9f5d2a31e012e0d8dbd11
David Leyva <dleyvaabrahantes@gmail.com>
2023-04-12 12:16:19 -0400

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by David on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
